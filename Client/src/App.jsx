ab25c898b3d9836119ed45ca0531246debd408e8
nanc77zion <ebnancy710@gmail.com>
2024-02-15 12:50:55 +0100

    created my react app

---
+++ Client/src/App.jsx
@@ -0,0 +1,36 @@
+
+import Register from './components/Register'
+import Login from './components/Login'
+import Faq from './components/Faq'
+import About from './components/About'
+import ErrorPage from './components/ErrorPage'
+import Dashboard from './components/Dashboard'
+import ProtectedRoutes from './components/ProtectedRoutes'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import SharedOutlet from './components/SharedOutlet'
+import './app.css'
+
+
+
+function App() {
+
+  return (
+      <Routes className='container-fluid'>
+      <Route path={'/'} element={<SharedOutlet />}>
+        <Route index element={<Home />} />
+        <Route path={'register'} element={<Register />} />
+        <Route path={'login'} element={<Login />} />
+        <Route path={'About'} element={<About />} />
+        <Route path={'faqs'} element={<Faq/>} />
+        <Route element={<ProtectedRoutes />} >
+          <Route path={'dashboard'} element={<Dashboard/>} />
+        </Route>
+      </Route>
+      <Route path={'*'} element={<ErrorPage />} />
+    </Routes>
+    
+  )
+}
+
+export default App
---
+++ Client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
