ae209f6814e79a0f67be5a3560096cc016dd40ff
nanc77zion <ebnancy710@gmail.com>
2024-02-16 15:35:51 +0100

    added sharedRoutes

--- Client/src/components/SharedRoutes.jsx
+++ Client/src/components/SharedRoutes.jsx
@@ -1,53 +1,58 @@
 //import React from 'react'
 import { Outlet,Link } from 'react-router-dom'
-import { FaTwitter,FaLinkedin, FaCopyright, FaFacebook, FaInstagram, FaSlack} from 'react-icons/fa'
+import { FaTwitter,FaLinkedin,FaFacebook, FaInstagram, FaSlack} from 'react-icons/fa'
 import { FaHome, FaPhone, FaMailBulk } from 'react-icons/fa'
 import Header from './Header'
 
 const SharedRoutes = () => {
   return (
     <>
-    <Header />
-    <Outlet/>
-    <div className='footer-cont container-fluid container-fluid-sm'>
-      <div className='footer row'>
-        <p  className='col-sm-6 pt-3'>Get connected with us on social networks: </p>
-        <div  className='col-sm-6 px-md-5 text-md-end' >
-          <span className=' px-2'><a className='text-white' href=''><FaTwitter /></a></span>
-          <span className=' px-2'><a className='text-white' href=''><FaLinkedin /></a></span>
-          <span className=' px-2'><a className='text-white' href=''><FaFacebook /></a></span>
-          <span className=' px-2'><a className='text-white' href=''><FaInstagram /></a></span>
-          <span className=' px-2'><a className='text-white' href=''><FaSlack /></a></span>
+      <Header />
+      <div className='bg-emerald-100'>
+        <Outlet />
+      </div>
+
+      <div className='footer-cont bg-amber-600 py-8'>
+        <div className='container mx-auto px-4'>
+          <div className='footer grid grid-cols-1 md:grid-cols-2'>
+            <p className='md:col-span-1 pt-3 md:pt-0 text-center md:text-left'>Get connected with us on social networks:</p>
+            <div className='flex justify-center md:justify-end md:col-span-1 px-4'>
+              <span className='px-2'><a className='text-white' href=''><FaTwitter /></a></span>
+              <span className='px-2'><a className='text-white' href=''><FaLinkedin /></a></span>
+              <span className='px-2'><a className='text-white' href=''><FaFacebook /></a></span>
+              <span className='px-2'><a className='text-white' href=''><FaInstagram /></a></span>
+              <span className='px-2'><a className='text-white' href=''><FaSlack /></a></span>
+            </div>
+          </div>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-8'>
+            <div className='px-4'>
+              <div className='flex items-center'>
+                <h2 className='text-3xl text-white'>T</h2>
+                <h2 className='text-3xl text-white ml-1'>Tech<span className='text-success'>Girls</span></h2>
+              </div>
+              <p className='mt-4 text-white'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam vitae mollitia tenetur corporis. Natus ex nemo dicta obcaecati, sapiente, ut voluptate ratione modi iure accusantium similique</p>
+            </div>
+            <div className='text-center'>
+              <h4 className='text-xl text-white'>Quick links</h4>
+              <div className='mt-4'>
+                <Link to='/about' className='block text-white hover:underline'>About</Link>
+                <Link to='/courses' className='block text-white hover:underline mt-2'>Courses</Link>
+                <Link to='/faqs' className='block text-white hover:underline mt-2'>FAQs</Link>
+              </div>
+            </div>
+            <div className='text-center'>
+              <h4 className='text-xl text-white'>Contact us</h4>
+              <p className='text-white mt-4'><FaHome /> 7 New Haven Road, Lagos. Nigeria</p>
+              <p className='text-white'><FaPhone /> (+234) 7086419364</p>
+              <p className='text-white'><FaMailBulk /> <a href='mailto:support@techgirls.com' className='hover:underline'>support@techgirls.com</a></p>
+            </div>
+          </div>
+          <div className='text-center mt-8 text-white'>&copy; 2023 Copyright TechGirls.com</div>
         </div>
       </div>
-      <div className='row'>
-        <div className='col-md-4 px-4'>
-          <div className='d-flex px-3 pt-4'>
-          <h2 className='foot-h3'>T</h2>
-          <h2 className='foot-h2'>Tech<span className='text-success'>Girls</span></h2>
-          </div>
-          <p className='pt-3 px-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam vitae mollitia tenetur corporis. Natus ex nemo dicta obcaecati, sapiente, ut voluptate ratione modi iure accusantium similique</p>
-        </div>
-        <div className='col-md-4 text-center py-4'>
-          <h4>Quick links</h4>
-          <div className='d-flex flex-column'>
-            <Link className='pt-2 text-decoration-none text-black'>About</Link>
-            <Link className='pt-2 text-decoration-none text-black'>Courses</Link>
-            <Link className='pt-2 text-decoration-none text-black'>FAQs</Link>
-          </div>
-        </div>
-        <div className='col-md-4 text-center py-4'>
-          <h4>Contact us</h4>
-          <p><FaHome /> 7 New Haven Road, Lagos. Nigeria</p>
-          <p><FaPhone /> (+234) 7086419364</p>
-          <p><FaMailBulk /> <a href='mailto:'> support@techgirls.com</a></p>
-        </div>
-      </div>
-      <div className='text-center '><FaCopyright /> 2023 Copyright TechGirls.com</div>
-      
-    </div>
     </>
-  )
-}
+  );
+};
+
 
 export default SharedRoutes
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -3,18 +3,17 @@ import lady from '../assets/lady.png'
 
 const Home = () => {
   return (
-    <div className='container-fluid-lg'>
-      <div className='row mx-0 px-4 my-0 bg-emerald-100'>
-      <div className='col mt-4 mt-md-5 '>
-        <h1 className=''>Welcome to Tech<span className='text-success'>Girls</span></h1>
-        <h5>Now learning from anywhere, and build a <span className='text-success text-decoration-underline' style={{fontFamily: 'sans-serif'}}>bright career</span></h5>
-        <p className='pt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam vitae mollitia tenetur corporis. Natus ex nemo dicta obcaecati, sapiente, ut voluptate ratione modi iure accusantium similique, cupiditate dolores. Corrupti, dolorem!</p>
+    <div className='container mx-auto px-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 md:mt-5'>
+        <div className='mt-4 md:mt-0'>
+          <h1 className='text-3xl md:text-4xl font-bold mb-2'>Welcome to Tech<span className='text-success'>Girls</span></h1>
+          <h5 className='text-base md:text-lg'>Now learning from anywhere, and build a <span className='text-success underline font-sans'>bright career</span></h5>
+          <p className='pt-3 text-sm md:text-base'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam vitae mollitia tenetur corporis. Natus ex nemo dicta obcaecati, sapiente, ut voluptate ratione modi iure accusantium similique, cupiditate dolores. Corrupti, dolorem!</p>
+        </div>
+        <div className='mt-3 md:mt-0'>
+          <img src={lady} alt='lady' className='w-full' />
+        </div>
       </div>
-      <div className='lady col mt-3'>
-        <img src={lady} alt='lady' />
-      </div>
-      
-    </div>
     </div>
     
   )
