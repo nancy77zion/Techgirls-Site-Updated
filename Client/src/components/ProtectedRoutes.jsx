8b2a615213158167d28467fc2baa77d94f7e41ec
nanc77zion <ebnancy710@gmail.com>
2024-05-08 14:44:49 +0100

    protected profile page

--- Client/src/components/ProtectedRoutes.jsx
+++ Client/src/components/ProtectedRoutes.jsx
@@ -1,11 +1,40 @@
 //import React from 'react'
+
 import {Outlet, Navigate} from 'react-router-dom'
 
 const ProtectedRoutes = () => {
+ 
+  const username = sessionStorage.getItem('user_id')
 
-  const username = sessionStorage.getItem('username')
+  return username ? <Outlet /> : <Navigate to={'/profile'} />
 
-  return username ? <Outlet /> : <Navigate to={'/login'} />
 }
 
 export default ProtectedRoutes
+
+
+
+
+
+
+
+
+
+
+
+// const role = sessionStorage.getItem('role');
+
+// // Check if the user is logged in and has a role
+// if (role) {
+//   // Check the user's role and redirect accordingly
+//   if (role === 'admin') {
+//     return <Outlet />;
+//   } else {
+//     // Redirect non-admin users to a different route
+//     return <Navigate to={'/dashboard'} />;
+//   }
+// } else {
+//   // Redirect users who are not logged in to the login page
+//   return <Navigate to={'/login'} />;
+// }
+
